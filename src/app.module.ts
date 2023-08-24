397c987cb3cc24c7d355a43bd9d1e25454559f09
arny-wilk <arnywilk@gmail.com>
2023-08-24 22:34:27 +0200

    adding task and delete task features done

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TodolistComponent } from "./app/component/todolist/todolist.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { FormsModule } from "@angular/forms";
+import { TodoComponent } from './app/component/todo/todo.component';
 
 @NgModule({
-    declarations: [AppComponent, TodolistComponent],
+    declarations: [AppComponent, TodolistComponent, TodoComponent],
     imports: [BrowserModule, FormsModule],
     bootstrap: [ AppComponent]
 })
--- src/app/component/todolist/todolist.component.ts
+++ src/app/component/todolist/todolist.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./todolist.component.css'],
 })
 export class TodolistComponent {
+  todo: string = 'se reveiller';
+
   taskList: string[] = ['manger', 'coder', 'faire les courses', 'dormir'];
+
+  onSubmit(todo: string) {
+    this.taskList.push(todo);
+    console.log(`We addd the todo in the list : `, this.taskList);
+  }
+
+  updateTask(newTodo: string, todo: string) {
+    if (this.taskList.includes(todo)) {
+      newTodo = todo;
+    }
+  }
+
+  onDelete() {
+     this.taskList.pop();
+    console.log(`${this.taskList}`);
+  }
 }
