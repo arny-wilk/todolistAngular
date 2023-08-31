271b5eb7e5c4727a04b87fb2c81c3ae46b236b6d
arny-wilk <arnywilk@gmail.com>
2023-08-31 21:55:21 +0200

    fix(user.service)amelioration des params d'url

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Services } from './todos.service';
 import { User } from '../models/user';
 import { environment } from 'src/environments/environment.development';
 import { Todo } from '../models/todo';
@@ -34,31 +33,35 @@ export class UserService {
   }
 
   public findTodosByUser(user: User) {
-    return this._http.get(this._baseUrl + `/${user.id}/todos`);
+    const hashed = this.hashed(user.todos);
+    return this._http.get(this._baseUrl + `/${user.id}/${hashed}`);
 }
 
   public findTodoByUser(user: User, todo: Todo[]) {
+    const hashed = this.hashed(todo);
     return this._http.get(
       this._baseUrl +
         `/${user.id}` +
-        `?todos=${user.todos === todo ? true : false}`
+        `?todos=${hashed}`
     );
   }
 
   public createTodo(user: User, createTodo: Todo[]) {
+    const hashed = this.hashed(createTodo);
     return this._http.post(
       this._baseUrl +
         `${user.id}` +
-        `?todo=${user.todos === createTodo ? true : false}`,
+        `?todo=${hashed}`,
       createTodo
     );
   }
 
   public updateTodo(user: User, updatedTodo: Todo[]) {
+    const hashed = this.hashed(updatedTodo);
     return this._http.put(
       this._baseUrl +
         `/${user.id}` +
-        `?udpated=${user.todos === updatedTodo ? true : false}`,
+        `?udpated=${hashed}`,
       updatedTodo
     );
   }
@@ -71,10 +74,15 @@ export class UserService {
   }
 
   public deleteAllTodos(user: User, deleteAllTodos: Todo[] ) {
+    const hashed = this.hashed(deleteAllTodos);
     return this._http.delete(
       this._baseUrl +
         `/${user.id}` +
-        `?deleteAll=${deleteAllTodos}`
+        `?deleteAll=${hashed}`
     );
-  }
+}
+
+public hashed(variable: any) {
+  return window.crypto.getRandomValues(variable);
+}
 }
